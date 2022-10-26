f1195311701bac889beef46a77a603eee2517272
adrianc68 <angeladriancamalgarcia@hotmail.com>
2022-10-26 17:19:56 -0500

    LGN-refactor: change folder structure
    Creation of validators folder

--- src/routers/userAccount.js
+++ src/routers/userAccount.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { createUser, deleteUserByUsername } = require("../controllers/userAccount");
-const { formatValidationAccountData, formatValidationAccountEmail, formatValidationAccountUsername } = require('../controllers/userAccountFormatValidator');
-const { validationIsUsernameRegisteredWithNext, validationisEmailRegisteredWithNext, validationIsEmailRegistered, validationIsUsernameRegistered } = require('../controllers/userAccountValidation');
+const { createUser, deleteUserByUsername } = require("../controllers/userAccountController");
+const { formatValidationAccountData, formatValidationAccountEmail, formatValidationAccountUsername } = require('../validators/formatValidators/userAccountFormatValidator');
+const { validationIsUsernameRegisteredWithNext, validationisEmailRegisteredWithNext, validationIsEmailRegistered, validationIsUsernameRegistered } = require('../validators/userAccountValidation');
 
 router.post("/accounts/create", formatValidationAccountData, validationIsUsernameRegisteredWithNext, validationisEmailRegisteredWithNext, createUser);
 router.delete("/accounts/username/delete", formatValidationAccountUsername, deleteUserByUsername);
--- src/validators/formatValidators/userAccountFormatValidator.js
+++ src/validators/formatValidators/userAccountFormatValidator.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { check, param, body } = require('express-validator');
-const { httpResponseValidation } = require('../helpers/httpResponses');
+const { httpResponseValidation } = require('../../helpers/httpResponses');
 
 const validateEmailData = [
     check("email")
