46f1a52bc88a8c8d9bac75d8365b6b14430be279
adrianc68 <angeladriancamalgarcia@hotmail.com>
2022-11-05 19:36:09 -0600

    LGN-refactor: change methods into tokendataaccess file

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,85 +1,6 @@
+const { removeOptionalAccessToken, refreshAccessToken, verifyToken, removeToken, generateTokens } = require("../dataaccess/tokenDataAccess");
 const { getAccountLoginData, getAccountLoginDataById } = require("../dataaccess/UserDataAccess");
-const { httpResponseOk, httpResponseInternalServerError, httpResponseUnauthorized, httpResponseErrorToken } = require("../helpers/httpResponses");
-const { logger } = require("../helpers/logger");
-const { addToken, removeToken, generateRefreshToken, generateAccessToken, verifyToken, TOKEN_TYPE } = require("../helpers/token");
-/**
- * It generates the accessToken and refreshToken
- * @param {*} username data to be save into payload.
- * @param {*} userId data to be save into payload.
- * @param {*} userRole data to be save into payload.
- * @returns the accessToken and refreshToken as JSON object.
- */
-const generateTokens = async (username, userId, userRole) => {
-    let accessToken;
-    let refreshToken;
-    try {
-        let payloadAccessToken = {
-            id: userId,
-            username,
-            userRole,
-        };
-        refreshToken = await generateRefreshToken(payloadAccessToken);
-        accessToken = await generateAccessToken(payloadAccessToken, refreshToken.jti);
-        await addToken(refreshToken.token, refreshToken.jti);
-        await addToken(accessToken.token, accessToken.jti);
-    } catch (error) {
-        removeToken(accessToken.jti);
-        removeToken(refreshToken.jti);
-        throw new Error(error);
-    }
-    let tokensCreated = {
-        refreshToken: refreshToken.token,
-        accessToken: accessToken.token
-    }
-    return tokensCreated;
-}
-/**
- * It generates a new accessToken using a refreshtoken identifier.
- * @param {*} username data to be save into payload.
- * @param {*} userId data to be save into payload.
- * @param {*} userRole data to be save into payload.
- * @param {*} refreshTokenJti the identifier of refreshToken
- * @returns the new accessToken as JSON object.
- */
-const refreshAccessToken = async (username, userId, userRole, refreshTokenJti) => {
-    let accessToken;
-    try {
-        let payloadAccessToken = {
-            userId,
-            username,
-            userRole,
-        };
-        accessToken = await generateAccessToken(payloadAccessToken, refreshTokenJti);
-        await addToken(accessToken.token, accessToken.jti);
-    } catch (error) {
-        removeToken(accessToken.jti);
-        throw new Error(error);
-    }
-    let tokenCreated = {
-        accessToken
-    }
-    return tokenCreated;
-}
-/**
- * It receives a optional accessToken and removes if exists from redis server.
- * @param {*} optionalAccessToken the accessToken to be removed
- * @returns undefined if token does not exist and fail message if cannot be removed from redis server
- */
-const removeOptionalAccessToken = async (optionalAccessToken) => {
-    let optionalOldAccessTokenMessage;
-    if (optionalAccessToken) {
-        try {
-            await verifyToken(optionalAccessToken);
-            await removeToken(optionalAccessToken);
-        } catch (error) {
-            optionalOldAccessTokenMessage = {
-                message: "Failed to remove access token",
-                errorType: error.message
-            }
-        }
-    }
-    return optionalOldAccessTokenMessage;
-}
+const { httpResponseOk, httpResponseInternalServerError, httpResponseErrorToken } = require("../helpers/httpResponses");
 
 const createTokens = async (request, response) => {
     let { emailOrUsername } = request.body;
@@ -129,8 +50,7 @@ const checkAuth = async (request, response, next) => {
     let tokenData;
     try {
         tokenData = await verifyToken(accessToken.split(" ")[1]);
-        if (tokenData.id) {
-            logger.debug(tokenData);
+        if (tokenData.userId) {
             return next();
         } else {
             return httpResponseErrorToken(response, "token is required to access to this resource!");
--- src/validators/authenticationValidation.js
+++ src/validators/authenticationValidation.js
@@ -1,13 +1,8 @@
 const { httpResponseInternalServerError, httpResponseNotFound, httpResponseErrorToken, httpResponseForbidden } = require("../helpers/httpResponses");
 const { encondePassword } = require("../helpers/cipher");
-const { checkToken, verifyToken, TOKEN_STATE, TOKEN_TYPE } = require("../helpers/token");
 const { getAccountLoginData } = require("../dataaccess/UserDataAccess");
+const { getTokenExist, TOKEN_TYPE } = require("../dataaccess/tokenDataAccess");
 
-/**
- * Check if user is not undefined or null.
- * @param {*} user 
- * @returns true if user has content otherwise false.
- */
 const doesExistUser = (user) => {
     let doesExistUser = false;
     if (user != undefined && user.length != 0) {
@@ -16,48 +11,12 @@ const doesExistUser = (user) => {
     return doesExistUser;
 };
 
-/**
- * Check if passwordA is equal to passwordB.
- * @param {*} passwordA 
- * @param {*} passwordB 
- * @returns true if both are equals otherwise false.
- */
 const doesPasswordMatch = (passwordA, passwordB) => {
     let doesPasswordMatch = false;
     if (passwordA == passwordB) {
         doesPasswordMatch = true;
     }
     return doesPasswordMatch;
-};
-
-/**
- * Check that the token has the VALID status and TokenType 
- * is the expected tokenType
- * @param {*} token token to be verified.
- * @param {*} tokenType type of token expected
- * @returns the actual value got from redis database (VALID|INVALID|NILL|Undefined|null)
- */
-const getTokenExist = async (token, tokenType) => {
-    let value;
-    try {
-        value = await checkToken(token);
-    } catch (error) {
-        throw new Error(error, `${tokenType} not found`)
-    }
-    if (!value || value == TOKEN_STATE.NIL) {
-        throw new Error(`${tokenType} does not exist`);
-    } else if (value.split(" ")[0] == TOKEN_STATE.INVALID) {
-        throw new Error(`${tokenType} has expired`)
-    }
-    try {
-        let values = await verifyToken(token);
-        if (values.tokenType != tokenType) {
-            throw (`you must provide a token of type ${tokenType} `)
-        }
-    } catch (error) {
-        throw new Error(error, `${tokenType} is not valid`);
-    }
-    return value;
 };
 
 const validationLoginData = async (request, response, next) => {
