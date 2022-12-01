6c8b10e7fa362afa7964cb3057396cee5fc165ea
adrianc68 <angeladriancamalgarcia@hotmail.com>
2022-12-01 00:30:07 -0600

    USR-refactor: remove data folder

--- src/server.js
+++ src/server.js
@@ -17,7 +17,6 @@ const server = app.listen({
         await connetionToServers();
         logger.info(`NodeJS Express Server initialized on port ${app.get("port")}`);
         logger.info("*******************************************************");
-        await clearDatabase();
     }
 });
 
